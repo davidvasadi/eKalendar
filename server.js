4838d1ec970e0dcc26b6d895b03c5d4feb8e39ed
Hnorbert95 <ho.norbert95@gmail.com>
2022-09-13 01:41:28 +0200

    Regisztrációs adatok átadása backendnek, siker

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
 
 //ez kell neked: mongoose.connect("mongodb+srv://testKal:testKal@cluster0.veozyk4.mongodb.net/?retryWrites=true&w=majority");
 // ez csak teszt:
 mongoose.connect('mongodb+srv://tut_db_user:M-login576@tutorialcluster.bgnihru.mongodb.net/?retryWrites=true&w=majority');
 
 const app= express();
+
+app.use(cors());
 app.use(express.static("templates"));
-app.use(express.json());
+app.use(express.urlencoded({extended:false})); //->middlerware hogy normálisan átmenjenek az adatok
+app.use(express.json());//->middlerware hogy normálisan átmenjenek az adatok
+
 
 app.get('/', (req, res) => {
     res.send('Itt lesz majd a login');
 })
 
 //reg útvonal
-app.get('/api/registration', (req,res) => {
-    res.send('Reg kezelés');
+app.post('/api/registration', (req,res) => {
+    console.log(req.body);
+    res.json({
+        fname: req.body.fname,
+        email: req.body.email,
+        coname: req.body.coname,
+        pwd: req.body.pwd
+    });
 })
 
 
